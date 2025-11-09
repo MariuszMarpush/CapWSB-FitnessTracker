777267c69a46968d6ecf5bf9645fd8b9a68f63e6
MariuszMarpush <mariusz.kuzmicki92@gmail.com>
2025-11-09 13:52:41 +0100

    Zadanie 7 zmiany w n-cjach

---
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/Healthmetrics.java
@@ -0,0 +1,54 @@
+package pl.wsb.fitnesstracker.healthmetrics;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import pl.wsb.fitnesstracker.user.api.User;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
+
+public class Healthmetrics {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Nullable
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "weight", nullable = false)
+    private double weight;
+
+    @Column(name = "height", nullable = false)
+    private double height;
+
+    @Column(name = "heartrate", nullable = false)
+    private Integer heartrate;
+
+    public Healthmetrics(
+            final LocalDate date,
+            final double weight,
+            final double height,
+            final Integer heartrate){
+            this.date = date;
+            ;
+            this.weight = weight;
+            this.height = height;
+            this.heartrate = heartrate;
+        }
+}
